7ad9b69601ec8d58baf28269ff180d9765e22a2a
jiejiejiang2004 <zhouwuqimc@163.com>
2026-04-08 18:11:25 +0800

    项目初始化

---
+++ src/test/java/main/xiaoqihui/XiaoQiHuiApplicationTests.java
@@ -0,0 +1,13 @@
+package main.xiaoqihui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XiaoQiHuiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
